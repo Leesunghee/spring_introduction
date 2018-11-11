71440811cebcaf3ee69b6cf1233fc691c4e4d9d1
Leesunghee <realwunz@gmail.com>
2018-11-11 18:49:23 +0900

    spring di practice by xml based configuration

--- src/main/java/com/ready2die/config/AppConfig.java
+++ src/main/java/com/ready2die/config/AppConfig.java
@@ -4,7 +4,7 @@ import com.ready2die.spring_introduction.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class AppConfig {
 
     @Bean
--- src/main/java/com/ready2die/spring_introduction/SpringIntroductionApplication.java
+++ src/main/java/com/ready2die/spring_introduction/SpringIntroductionApplication.java
@@ -11,8 +11,8 @@ public class SpringIntroductionApplication {
 
     public static void main(String[] args) {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext("com.ready2die.config");
-//        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+//        ApplicationContext context = new AnnotationConfigApplicationContext("com.ready2die.config");
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
         UserService userService = context.getBean(UserService.class);
 
         UserRepository userRepository = context.getBean(UserRepository.class);
