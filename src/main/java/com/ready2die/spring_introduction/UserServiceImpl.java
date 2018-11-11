e18b9e78d91dd3f11f84c195f42ed7b969c465fb
Leesunghee <realwunz@gmail.com>
2018-11-11 20:33:35 +0900

    di practice by field injection

--- src/main/java/com/ready2die/spring_introduction/UserServiceImpl.java
+++ src/main/java/com/ready2die/spring_introduction/UserServiceImpl.java
@@ -6,14 +6,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserServiceImpl implements UserService {
 
-    private final PasswordEncoder passwordEncoder;
-    private final UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+    private UserRepository userRepository;
+
+//    @Autowired
+//    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+//        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+//    }
 
     @Override
     public void register(User user, String rawPassword) {
