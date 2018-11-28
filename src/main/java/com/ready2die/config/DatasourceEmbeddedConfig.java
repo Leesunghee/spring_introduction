7ce9abc3b1b2a41cec4f30c34729eaa8032361cf
Leesunghee <realwunz@gmail.com>
2018-11-28 22:13:35 +0900

    set datasource

---
+++ src/main/java/com/ready2die/config/DatasourceEmbeddedConfig.java
@@ -0,0 +1,22 @@
+package com.ready2die.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceEmbeddedConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding("UTF-8")
+                .addScripts("META-INF/sql/schema.sql", "META-INF/sql/insert-init-data.sql")
+                .build();
+    }
+}
